408f0076cb6c4b9457b5e0f013e7916257937f3b
Tomislav Kovačević <tomy.kovacevic@gmail.com>
2025-06-14 18:50:52 +0200

    Added a sentence that clarifies what is being shown

--- isArmstrong/src/Main.java
+++ isArmstrong/src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     }
 
     private static void isArmstrong() {
-        boolean firstNum = true;
+        boolean firstNum = true; // used to avoid comma at the end of the listed numbers while not using an array
+        System.out.print("Armstrong numbers in a range from 1 to 10000 are: ");
         for (int i = 0; i < 10000; i++) {
             int sum = 0;
             int number = i;
