fd94d8aa5170b7eab4654dafb7eaca260bdd134b
Tomislav Kovačević <tomy.kovacevic@gmail.com>
2025-06-17 16:31:14 +0200

    Finished method checkOIB that checks OIB validity, added dictionary for croatian words to IntelliJ natural languages

--- checkOIB - zadatak02/src/Main.java
+++ checkOIB - zadatak02/src/Main.java
@@ -4,8 +4,45 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Unesite OIB:");
-        String input = sc.next();
+        String oib = sc.next();
 
+        if (oib.length() != 11) {
+            System.out.println("OIB mora sadržati 11 znamenki.");
+            return;
+        }
+        checkOIB(oib);
+    }
 
+    private static void checkOIB(String oib) {
+        int controlNumber = 0;
+        for (int i = 0; i < oib.length() - 1; i++) {
+            int digit = Character.getNumericValue(oib.charAt(i));
+            if (i == 0) {
+                controlNumber = digit + 10;
+            } else {
+                controlNumber += digit;
+            }
+
+            if (controlNumber % 10 == 0) {
+                controlNumber = 10;
+            } else {
+                controlNumber %= 10;
+            }
+
+            controlNumber *= 2;
+            controlNumber %= 11;
+        }
+
+        if (controlNumber == 1) {
+            controlNumber = 0;
+        } else {
+            controlNumber = 11 - controlNumber;
+        }
+
+        if (controlNumber == Character.getNumericValue(oib.charAt(10))) {
+            System.out.println("Uneseni OIB je valjan.");
+        } else {
+            System.out.println("Uneseni OIB nije valjan.");
+        }
     }
 }
