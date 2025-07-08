9b2e36bb262605299131e8662103adf0fc772bc0
Tomislav Kovačević <tomy.kovacevic@gmail.com>
2025-07-08 17:18:54 +0200

    Removed unused imports from Vozilo class

--- parcijalniIspitVjezba/src/Vozilo.java
+++ parcijalniIspitVjezba/src/Vozilo.java
@@ -1,9 +1,3 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 public abstract class Vozilo {
     private final String registarskiBroj, marka;
     private final short godinaProizvodnje;
